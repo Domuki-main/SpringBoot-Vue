dd1c200986acb14594d99134f6677467b1a39095
MA ZHIYUAN <59246869+Domuki-main@users.noreply.github.com>
2022-03-25 11:01:08 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,18 @@
 <template>
-    
+	<div style="height: 50px; 
+	line-height:50px;
+	border-bottom: 1px solid #ccc; 
+	display: flex;">
+		<div style="width: 200px;">Management</div>
+		<div style="flex: 1;"></div>
+		<div style="width: 100px;">drop down</div>
+	</div>
 </template>
 
 <script>
-export default {
-    name: "Header"
-}
+	export default {
+		name: "Header"
+	}
 </script>
 
 <style scoped>
