001ce0964d068988370c8757ae5f59005f438428
MA ZHIYUAN <59246869+Domuki-main@users.noreply.github.com>
2022-03-25 12:17:05 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,18 +3,47 @@
 	line-height:50px;
 	border-bottom: 1px solid #ccc; 
 	display: flex;">
-		<div style="width: 200px;">Management</div>
+		<div style="width: 200px; 
+		padding-left: 30px; 
+		font-weight: bold; 
+		color: dodgerblue;">Management</div>
 		<div style="flex: 1;"></div>
-		<div style="width: 100px;">drop down</div>
+		<div style="width: 100px;">
+			<el-dropdown>
+				<span class="el-dropdown-link">
+					<br>Domuki
+					<el-icon class="el-icon--right">
+						<arrow-down />
+					</el-icon>
+				</span>
+				<template #dropdown>
+					<el-dropdown-menu>
+						<el-dropdown-item>Personal Information</el-dropdown-item>
+						<el-dropdown-item>Logout</el-dropdown-item>
+					</el-dropdown-menu>
+				</template>
+			</el-dropdown>
+		</div>
 	</div>
 </template>
 
 <script>
+	import {
+		ArrowDown
+	} from '@element-plus/icons-vue'
 	export default {
-		name: "Header"
+		name: "Header",
+		components: {
+			ArrowDown
+		}
 	}
 </script>
 
 <style scoped>
-
+	.el-dropdown-link {
+		cursor: pointer;
+		color: var(--el-color-primary);
+		display: flex;
+		align-items: center;
+	}
 </style>
